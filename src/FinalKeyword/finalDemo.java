552c53952fecb4e44293f4f2a74bca4e0d04ec92
Ajay <ajay.gupta1711@gmail.com>
2022-03-29 21:17:45 +0530

    Static, Non-Static, this & final keywords

---
+++ src/FinalKeyword/finalDemo.java
@@ -0,0 +1,19 @@
+package FinalKeyword;
+import SuperKeyword.ChildDemo;
+import SuperKeyword.ParentDemo;
+
+public class finalDemo extends ChildDemo {
+
+	// if Child demo class, if used "final class ChildDemo extends ParentDemo {"
+	// Class name as final..that means you can not extend that class 
+	
+	
+	public static void main(String[] args) {
+		
+		final int i = 5;
+		
+//		i=5;
+
+	}
+
+}
